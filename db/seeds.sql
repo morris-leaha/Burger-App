2aa6b1912305034f20d17108e5008c48f0a956eb
LAMorris <morris.leaha@gmail.com>
2019-03-18 00:43:04 -0500

    initial db setup

---
+++ db/seeds.sql
@@ -0,0 +1,14 @@
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Big Mac", false);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Turkey Burger", false);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Bison Burger", false);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Veggie Burger", false);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Double Cheeseburger", false);
