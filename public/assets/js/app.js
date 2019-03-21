c511603c39af790af1435623e4fc16ec4fb3811f
LAMorris <morris.leaha@gmail.com>
2019-03-21 18:14:49 -0500

    initial launch

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -3,22 +3,69 @@
 $(function() {
 
     // FOR USER TO ADD A NEW BURGER:
-        // Attach $().on("click") event handler to user input field, when user hits submit button
+        // Attach $().on("submit") event handler to user input field, when user hits submit button
+        $(".create-burg-form").on("submit", function(event) {
             // Use event.preventDefault(); 
+            event.preventDefault(); 
             // "Grab" value of burger name from input field --> store in as object
+            var newBurg = {
+                burger_name: $("#userInputBurger").val().trim()
+            }
             // "Send" the new burger Object to db using ajax POST request
+            $.ajax("/api/burgers", {
+                type: "POST", 
+                data: newBurg
+            }).then(
+                function() {
+                    console.log("successfully created new burger");
+                    // reload to update page 
+                    location.reload();
+                }
+            );
+        });
     
     // FOR USER TO CHANGE THE STATUS OF A STORED BURGER (TO TOGGLE B/W DEVOURED STATE OF TRUE OR FALSE):
         // Attach $().on("click") event handler to the 'change-status' button
+        $(".change-status").on("click", function(event) {
             // "Grab" the id of that specific burger stored in db (held by the data-id value of button) --> store in variable
+            var id = $(this).data("id");
+
             // "Grab" the current boolean value of that specific burger (held by data-newdevourstate value of button) --> store in variable 
+            var newState = $(this).data("newdevourstate");
+
             // Create new object for updated devoured state --> store as object
+            var newState = {
                 // "Set" current devoured key value to value of current boolean
+                devoured: newState
+            };
+            
             // "Send" the new status to db using ajax PUT request
+            $.ajax("/api/burgers/" + id, {
+                type: "PUT", 
+                data: newState
+            }).then(
+                function() {
+                    console.log("changed devoured state to: " + newState);
+                    location.reload();
+                }
+            );
+        });
             
     // FOR USER TO DELETE A BURGER:
         // Attach $().on("click") event handler to the 'delete-btn' button 
+        $(".delete-btn").on("click", function(event) {
             // "Grab" the id of that specific burger stored in db (held by the data-id value of button) --> store in variable
+            var id = $(this).data("id");
+
             // Delete the burger by using ajax DELETE request
+            $.ajax("/api/burgers/" + id, {
+                type: "DELETE"
+            }).then(
+                function() {
+                    console.log("successfully deleted burger: " + id);
+                    location.reload();
+                }
+            );
+        });
 
 });
