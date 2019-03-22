b3066a65fc5a9833b0b1a7c737f08ae7a6c74d41
LAMorris <morris.leaha@gmail.com>
2019-03-21 23:19:21 -0500

    updated delete functionality

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -51,7 +51,7 @@ router.put("/api/burgers/:id", function(req, res) {
 router.delete("/api/burgers/:id", function(req, res) {
     var specLocation = "id = " + req.params.id;
 
-    burger.delete(specLocation, function(result) {
+    burger.deleteBurger(specLocation, function(result) {
         if (result.affectedRows == 0) {
           // If no rows were changed, then the ID must not exist, so 404
           return res.status(404).end();
